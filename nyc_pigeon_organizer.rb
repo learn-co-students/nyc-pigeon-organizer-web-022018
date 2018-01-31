411e067c107f24a56cc1fe8aa31f2be2f148e850
sinclairstudy <sinclairstudy@gmail.com>
2018-01-31 01:43:49 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  output = {}
+
+  data.each do |attribute_name, hash|
+    hash.each do |attribute_value, names|
+      names.each do |name|
+        if output[name].nil?
+          output[name] = {attribute_name => [attribute_value.to_s]}
+        elsif output[name][attribute_name].nil?
+          output[name][attribute_name] = [attribute_value.to_s]
+        else
+          output[name][attribute_name] << attribute_value.to_s
+        end
+      end
+    end
+  end
+
+  output
 end
